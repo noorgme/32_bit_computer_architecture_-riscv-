5b3b57436515d490d8fca0f059db0d6dfcd080b4
Patrick Beart <patrick.beart@gmail.com>
2022-12-02 23:38:30 +0000

    Minor fixes

--- test/samples/startlights/patrickproposal.riscv.s
+++ test/samples/startlights/patrickproposal.riscv.s
@@ -1,6 +1,6 @@
-addi a0, a0, 0b11111000000 # Set a0 to make all bits 1 except bottom 5 bits which = 0, so all lights are off but 
-addi x29, x29, 0b11111 # Use x29 to store what the value of a0 will be once the lights have counted up to full
-addi a1, x0, 20 # count up cycle count
+addi a0, a0, 0b1111100000 # Set a0 to make all bits 1 except bottom 6 bits which = 0, so all lights are off but we can change the lights by simply shifting right
+addi x29, x29, 0b11111 # Use x29 to store what the value of a0 will be once the lights have counted up to full (i.e. all lights are 1)
+addi a1, x0, 2 # count up cycle count
 
 light_display_loop:
 jal ra, subroutine_wait_a1
@@ -11,8 +11,8 @@ addi a1, x0, 50 # lights off cycle count
 jal ra, subroutine_wait_a1
 addi a0, x0, 0b0 # turn off lights
 
-stuck:
-jal x0, stuck
+end:
+jal x0, end
 
 subroutine_wait_a1:
 addi x28, a1, 0

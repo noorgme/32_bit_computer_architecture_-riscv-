e86bbefceb05fe490c8c06c07958e2e628c21ef8
jacksonbarlow <62945384+jacksonbarlow@users.noreply.github.com>
2022-12-03 16:34:54 +0000

    PCTestFile

---
+++ src/programCounter_tb.cpp
@@ -0,0 +1,70 @@
+#include "VpcTop.h"
+#include "verilated.h"
+#include "verilated_vcd_c.h"
+#include <stdlib.h>
+#include <iostream>
+
+
+
+int main(int argc, char **argv, char **env) {
+    int i;
+    int clk;
+    srand (time(NULL));
+
+    Verilated::commandArgs(argc, argv);
+    // init top verilog instance
+    VpcTop* top = new VpcTop;
+    // init trace dump
+    Verilated::traceEverOn(true);
+    VerilatedVcdC* tfp = new VerilatedVcdC;
+    top->trace (tfp, 99);
+    tfp->open ("programCounter.vcd");
+
+    // init sim inputs
+    top->clk = 1;
+    top->rst = 0;
+    top->PCsrc = 0;
+    top->ImmOp = 0;
+
+    // run sim for many clock cycles
+    for (i=0; i< 10000; i++) {
+
+        // dump vars into vcd file & toggle clock
+        for (clk=0; clk<2; clk++) {
+            tfp->dump(2*i+clk);
+            top->clk = !top->clk;
+            top->eval();
+        }
+
+        if(top->count == 5000) {
+            top->rst = 1;
+        }
+
+        if(top->count == 1000) {
+            top->rst = 0;
+        }
+
+        if(top->count == 1500) {
+            top->PCsrc = 1;
+        }
+
+        if(top->count == 2000) {
+            top->rst = 1;
+        }
+
+        if(top->count == 2001) {
+            top->rst = 0;
+            top->ImmOp = 4294967295;
+        } else {
+            top->ImmOp = rand() % RAND_MAX;
+        }
+        
+        if(top->count == 2002) {
+            top->PCsrc = 0;
+        }
+
+        std::cout << i << " - PC: " << top->pc << std::endl;
+
+        if (Verilated::gotFinish()) exit(0);
+    }
+}

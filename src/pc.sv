2f7e6efd612171e9d6c972be14d55ae2d7d30080
Jackson  Barlow <jacksonbarlow@jacksons-air.home>
2022-12-04 13:51:50 +0000

    PC Should Be Fixed Now, error with intermediate count sig & test program

--- src/pc.sv
+++ src/pc.sv
@@ -5,21 +5,20 @@ module pc #(
     input logic                                 clk,
     input logic                                 PCsrc,
     input logic                                 rst,
-    output logic    [DATA_WIDTH-1:0]            pc
+    output logic    [DATA_WIDTH-1:0]            pc,
+    output logic    [DATA_WIDTH-1:0]            count
 );
 
     logic           [DATA_WIDTH-1:0]            next_PC;
 
     always_ff @(posedge clk)
-        pc <= next_PC;
+        begin
+            pc <= next_PC;
+            count <= count + 1'b1;
+        end
 
     always_comb
-        if (rst == 1'b1){
-            next_PC = DATA_WIDTH'b0;
-        } else if (PCsrc == 1'b1) {
-            next_PC = {pc + ImmOp}[DATA_WIDTH-1:0];
-        } else {
-            next_PC = {pc + DATA_WIDTH'b100}[DATA_WIDTH-1:0];
-        }
-
+        if (rst == 1'b1)            next_PC = DATA_WIDTH'b0;
+        else if (PCsrc == 1'b1)     next_PC = {pc + ImmOp}[DATA_WIDTH-1:0];
+        else                        next_PC = {pc + DATA_WIDTH'b100}[DATA_WIDTH-1:0];
 endmodule

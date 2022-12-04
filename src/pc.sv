9647829374df33b7958c92d5ae4f5600dc9d1a68
Patrick Beart <patrick.beart@gmail.com>
2022-12-04 13:36:44 +0000

    Change logic name pc to pcounter because it conflicts with the module name

--- src/pc.sv
+++ src/pc.sv
@@ -5,21 +5,21 @@ module pc #(
     input logic                                 clk,
     input logic                                 PCsrc,
     input logic                                 rst,
-    output logic    [DATA_WIDTH-1:0]            pc
+    output logic    [DATA_WIDTH-1:0]            pcounter
 );
 
     logic           [DATA_WIDTH-1:0]            next_PC;
 
     always_ff @(posedge clk)
-        pc <= next_PC;
+        pcounter <= next_PC;
 
     always_comb
         if (rst == 1'b1) begin
             next_PC = 'b0;
         end else if (PCsrc == 1'b1) begin
-            next_PC = {pc + ImmOp}[DATA_WIDTH-1:0];
+            next_PC = {pcounter + ImmOp}[DATA_WIDTH-1:0];
         end else begin
-            next_PC = {pc + 'b100}[DATA_WIDTH-1:0];
+            next_PC = {pcounter + 'b100}[DATA_WIDTH-1:0];
         end
 
 endmodule

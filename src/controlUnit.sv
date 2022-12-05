c9e275720e759e51a4a121aa61e5b9a58a7e44a1
Noor Elsheikh <noorcatch@gmail.com>
2022-12-05 16:50:50 +0000

    Working ALU decoder, main decoder, control unit - with respective testbenches

---
+++ src/controlUnit.sv
@@ -0,0 +1,47 @@
+module controlUnit (
+//Inputs
+    input logic zero,
+    input logic [6:0] op,
+    input logic [2:0] funct3,
+    input logic funct7_5,
+//Outputs
+    output logic PCSrc, //Select PC:=PC+4 (Sel0) or PC:=PC+ImmOp (Sel1) 
+    output logic ResultSrc,
+    output logic MemWrite,
+    output logic [2:0] ALUControl,
+    output logic ALUSrc,
+    output logic [1:0] ImmSrc,
+    output logic RegWrite
+);
+wire BranchWire;
+wire [1:0] ALUOpWire;
+
+assign PCSrc = BranchWire & zero;
+
+
+mainDecoder mainDecoder(
+//Inputs
+    .op (op),
+//Outputs
+    .Branch (BranchWire),
+    .ImmSrc (ImmSrc),
+    .ALUSrc (ALUSrc),
+    .ResultSrc (ResultSrc),
+    .RegWrite (RegWrite),
+    .MemWrite (MemWrite),
+    .ALUOp (ALUOpWire)
+
+);
+
+aluDecoder aluDecoder (
+//Inputs
+    .funct3 (funct3),
+    .funct7_5 (funct7_5),
+    .op5 (op[5]),
+    .ALUOp (ALUOpWire),
+//Outputs
+    .ALUControl (ALUControl)
+);
+
+
+endmodule

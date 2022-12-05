986c4809606fe4c696bd302e8e4250dd44d2752e
jackson laptop <jacksonrbarlow@gmail.com>
2022-12-05 16:56:41 +0000

    Testbench & .sv for memory fixed

--- src/datamemory.sv
+++ src/datamemory.sv
@@ -2,21 +2,20 @@ module datamemory #(
     parameter   DATA_WIDTH = 32, 
                 ADDRESS_WIDTH = 16
 )(
-    input logic     [ADDRESS_WIDTH-1:0] A,
-    input logic     [DATA_WIDTH-1:0]    WD,
-    input logic                         WE,
+    input logic     [ADDRESS_WIDTH-1:0] address,
+    input logic     [DATA_WIDTH-1:0]    write_data,
+    input logic                         write_enable,
     input logic                         clk,
-    output logic    [DATA_WIDTH-1:0]    RD
+    output logic    [DATA_WIDTH-1:0]    read_data
 );
 
-logic [DATA_WIDTH-1:0] data_mem [2**ADDRESS_WIDTH-1:0];
+    logic [DATA_WIDTH-1:0] data_mem [2**ADDRESS_WIDTH-1:0];
 
-always_ff @(posedge clk) 
-    begin
-        if(WE == 1'b1)
-            data_mem [A] <= WD;
-        RD <= data_mem [A];
-    end
-
+    always_ff @(posedge clk) 
+        begin
+            read_data <= data_mem[address];
+            if (write_enable == 1'b1) 
+                data_mem[address] <= write_data;
+        end
 
 endmodule
--- src/datamemory_tb.cpp
+++ src/datamemory_tb.cpp
@@ -5,21 +5,20 @@
 int main (int argc, char **argv, char **env) {
     Verilated::commandArgs(argc, argv);
 
-    Vdatamemory* top = new Vdatamemory;
+    Vdatamemory * top = new Vdatamemory;
 
     Verilated::traceEverOn(true);
     VerilatedVcdC* tfp = new VerilatedVcdC;
     top->trace (tfp, 99);
     tfp->open ("datamemory.vcd");
 
-    top->clk = 0;
-    top->A = 0;
-    top->WD = 0;
-    top->WE = 0;
+    top->clk = 1;
+    top->address = 0;
+    top->write_data = 0;
+    top->write_enable = 0;
     int count = 0;
     int clk;
     int i;
-    int dout;
 
     for (i=0; i<500; i++) {
         for (clk=0; clk<2; clk++) {
@@ -28,20 +27,21 @@ int main (int argc, char **argv, char **env) {
             top->eval();
         }
 
-        top->A = count;
-        top->WD = 500 - i;
+        top->address = count;
+        top->write_data = 500 - i;
         
         if (i == 40) {
-            top->WE = 1;
+            top->write_enable = 1;
             count = 0;
         } else if (i == 60) {
-            top->WE = 0;
+            top->write_enable = 0;
             count = 0;
         }
         
         count = count + 1;
-        dout = top->RD;
         
         if (Verilated::gotFinish()) exit(0);
     }
+    tfp->close();
+    exit(0);
 }

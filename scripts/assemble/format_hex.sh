3cfba6d6f32c2dc519f3f27cac0f2929b2259177
Patrick Beart <patrick.beart@gmail.com>
2022-12-07 04:07:06 +0000

    Prevent systemverilog from incorrectly loading .mem file by removing whitespace when generating hex dumps of instruction memory

--- scripts/assemble/format_hex.sh
+++ scripts/assemble/format_hex.sh
@@ -8,4 +8,6 @@ pwd
 
 echo "$1"
 
-od -v -An -t x1 "$1.bin" | tr -s '\n' | awk '{$1=$1};1' > "$1.hex"
+#od -v -An -t x1 "$1.bin" | tr -d "\n" | tr -d " " | awk '{$1=$1};1' > "$1.hex"
+
+hexdump -ve '4/1 "%.2x" "\n"' "$1.bin" > "$1.hex"

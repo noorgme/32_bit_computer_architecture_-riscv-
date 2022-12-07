6dfa28eb135422d936019cecbda6199b1559715b
Patrick Beart <patrick.beart@gmail.com>
2022-12-07 13:08:44 +0000

    Reverse byte order within words to fix endianness problems

--- scripts/assemble/format_hex.sh
+++ scripts/assemble/format_hex.sh
@@ -10,4 +10,7 @@ echo "$1"
 
 #od -v -An -t x1 "$1.bin" | tr -d "\n" | tr -d " " | awk '{$1=$1};1' > "$1.hex"
 
-hexdump -ve '4/1 "%.2x" "\n"' "$1.bin" > "$1.hex"
+od -v -An -t x1 "$1.bin" | tr -s '\n' | awk '{$1=$1};1' > "$1_od.hex"
+
+# reverse .bin byte order, then hexdump, then reverse lines so only bytes within lines are reversed (fix endianness)
+< "$1.bin" xxd -p -c1 | tac | xxd -p -r | hexdump -ve '4/1 "%.2x" "\n"' | tac > "$1.hex"

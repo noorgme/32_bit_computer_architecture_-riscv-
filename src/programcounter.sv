3c5b1dd58b3c52591dc49b080f2b6fe09961e558
Noor Elsheikh <noorcatch@gmail.com>
2022-12-12 17:22:34 +0000

    Fixed issue with BNE

--- src/programcounter.sv
+++ src/programcounter.sv
@@ -16,7 +16,7 @@ module programcounter #(
         begin
         if (rst == 1'b1)            pc <= 'b0; //Reset PC
         else if (PCsrc == 2'b01)    pc <= {pc + ImmOp}[DATA_WIDTH-1:0]; //B-type generic and JAL
-        else if (PCsrc == 2'b10)    pc <= ALUresult;
+        else if (PCsrc == 2'b10)    pc <= ALUresult; //JALR
         else                        pc <= pcplus4; //Increment PC by 4
         end
         

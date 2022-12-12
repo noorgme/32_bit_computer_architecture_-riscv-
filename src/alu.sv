e1c16f46bfc24163898eb8fd6acc374ff559e7de
jackson laptop <jacksonrbarlow@gmail.com>
2022-12-12 17:50:32 +0000

    added xor to alu

--- src/alu.sv
+++ src/alu.sv
@@ -7,7 +7,7 @@ module alu #(
     output logic zero
 );
 
-    typedef enum [3:0]{ADD=0,SUB=1,AND=2,OR=3,SLT=5,SLL=6,SRL=7,SRA=8} alumodes;
+    typedef enum [3:0]{ADD=0,SUB=1,AND=2,OR=3,XOR=4,SLT=5,SLL=6,SRL=7,SRA=8} alumodes;
     int signed op1sin = op1, op2sin = op2;
 
     always_comb begin
@@ -16,6 +16,7 @@ module alu #(
             SUB:    aluout = {op1 - op2}[DATA_WIDTH-1:0];
             AND:    aluout = {op1 & op2};
             OR:     aluout = {op1 | op2};
+            XOR:    aluout = {op1 ^ op2};
             SLT:begin
                 if(op1<op2) aluout = op1;
                 else aluout = op2;
@@ -31,6 +32,7 @@ module alu #(
             SUB:    if(op1sin >= op2sin) zero = 1;
             AND:    if(op1 >= op2) zero = 1;
             OR:     if(op1sin < op2sin) zero = 1;
+            XOR:    if(op1sin < op2sin) zero = 1;
             SLT:    if(op1 < op2) zero = 1;
             SLL:    zero = 0;
             SRL:    zero = 0;

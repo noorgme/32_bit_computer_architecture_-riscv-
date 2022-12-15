c2ac9f438d836ce5f7ad6d1c5a6ecc7e0ece8410
splogdes <95136830+splogdes@users.noreply.github.com>
2022-12-15 16:31:06 +0000

    small improvments to datacontroller and antother test case

--- src/datacontroller.sv
+++ src/datacontroller.sv
@@ -1,43 +1,37 @@
 module datacontroller #(
     parameter   DATA_WIDTH = 32
 ) (
-    input logic [DATA_WIDTH-1:0] mem_data_in, mem_data_out, 
+    input logic [DATA_WIDTH-1:0] mem_data_in, 
+    input logic [3:0][DATA_WIDTH/4-1:0] mem_data_out, 
     input logic [2:0] DATAMEMControl,
     input logic [1:0] First_2,
-    output logic [DATA_WIDTH-1:0] write_data, read_data
+    output logic [3:0][DATA_WIDTH/4-1:0] write_data,
+    output logic [DATA_WIDTH-1:0] read_data
 );
 
-    logic [4:0]    shiftb = {First_2,3'b0},shifth = {First_2[1],4'b0};
-    logic [7:0]    tmpb_out;
-    logic [15:0]   tmph_out;
-    logic [DATA_WIDTH-1:0] tmp_data;
-
     always_comb begin
-        tmpb_out = {mem_data_out >> shiftb}[7:0];
-        tmph_out = {mem_data_out >> shifth}[15:0];
-        write_data = 32'b0;
-        read_data = 32'b0;
-        tmp_data = 0;
+        write_data = 0;
+        read_data = 0;
         case(DATAMEMControl)
             {3'b000}:begin
-                read_data = {{24{tmpb_out[7]}}, tmpb_out}; //lb
-                tmp_data = {{{mem_data_out>>shiftb|mem_data_out<<(DATA_WIDTH-shiftb)}[DATA_WIDTH-1:8],mem_data_in[7:0]}}[DATA_WIDTH-1:0];
-                write_data = tmp_data<<shiftb|tmp_data>>(DATA_WIDTH-shiftb); //sb
+                read_data = {{24{mem_data_out[First_2][7]}},mem_data_out[First_2]}; //lb
+                write_data = mem_data_out; 
+                write_data[First_2] =mem_data_in[7:0]; //sb
             end
             {3'b001}: begin
-                read_data = {{16{tmph_out[15]}},tmph_out}; //lh
-                tmp_data = {{{mem_data_out>>shifth|mem_data_out<<(DATA_WIDTH-shifth)}[DATA_WIDTH-1:16],mem_data_in[15:0]}}[DATA_WIDTH-1:0];
-                write_data = tmp_data<<shifth|tmp_data>>(DATA_WIDTH-shifth); //sh
+                read_data = {{16{mem_data_out[{First_2[1],1'b0}+:2][15]}},mem_data_out[{First_2[1],1'b0}+:2]}; //lh
+                write_data = mem_data_out;
+                write_data[{First_2[1],1'b0}+:2]=mem_data_in[15:0]; //sh
             end
             {3'b010}: begin
                 read_data = mem_data_out; //lw
-                write_data = mem_data_in; //sw
+                write_data =mem_data_in; //sw
             end
-            {3'b100}: read_data = {{24{1'b0}}, tmpb_out}; //lbu
-            {3'b101}: read_data = {{16{1'b0}},tmph_out}; //lhu
+            {3'b100}: read_data = {{24{1'b0}},mem_data_out[First_2]}; //lbu
+            {3'b101}: read_data = {{16{1'b0}},mem_data_out[{First_2[1],1'b0}+:2]}; //lhu
             default: begin
-                write_data = 32'b0;
-                read_data = 32'b0;
+                write_data = 0;
+                read_data = 0;
             end
         endcase
     end
--- test/testcases/dataunit.riscv.s
+++ test/testcases/dataunit.riscv.s
@@ -21,17 +21,15 @@ add s0, s0, s0
 li a2, -0x112234 #0xFFEEDDCC
 
 sw a2, 0(s0)
-lhu a0, 2(s0)
-lh a0, 0(s0)
-lb a0, 2(s0)
-lbu a0, 1(s0)
+lhu a3, 2(s0)
+lh a4, 0(s0)
+lb a5, 2(s0)
+lbu a6, 1(s0)
 addi t1, zero, -0x1
 addi s1, zero, 0x420
 lw s1, 0(s1)
 addi s0, zero, 0x20
-lw a0, 0(s0)
-li a0, -0x11111112
-
+lw a7, 0(s0)
 li t2, -0x11111112
 
 li t3, -0x22222223
@@ -43,3 +41,7 @@ sw t1, 0(s0)
 sw t3, 8(s0)
 sw t2, 4(s0)
 sw t4, 12(s0)
+lw s3, 0(s0)
+lw s4, 4(s0)
+lw s5, 8(s0)
+lw s6, 12(s0)

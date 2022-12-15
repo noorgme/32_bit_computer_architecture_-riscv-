b568c2142be297d49c4e14c8f20d5326748637bb
splogdes <95136830+splogdes@users.noreply.github.com>
2022-12-15 06:31:18 +0000

    ollies proposal done

--- test/samples/startlights/olliesproposal.riscv.s
+++ test/samples/startlights/olliesproposal.riscv.s
@@ -1,13 +1,9 @@
-.text
-.globl main
 addi s1, zero, 0x1 #place holder for 1
 addi s8, zero, 0x8 #place holder for 8
 main:
     addi a0, zero, 0x0 #restarts counter
     addi t2, zero, 0x0 #counter for lights
 lights:
-    lui t6, 0xff #set delay can be changed
-    jal ra, delay
     jal ra, output
     beq t2, s8, end
     jal lights
@@ -15,11 +11,7 @@ output:
     add t2, t2, s1 #adds 1 to t2
     sll t0, s1, t2 # 1 is shifted by t2 and stored in t0
     sub a0, t0, s1 # -1 from this and it is the output
-    jalr ra
-delay:  
-    sub t6, t6, s1 # counts down from the chosen delay
-    bne t6, zero, delay
-    jalr ra
+    ret
 end:
     jal end
 

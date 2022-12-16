1623ff62e3e163ca5f9318401fedceb67e2541e7
jackson laptop <jacksonrbarlow@gmail.com>
2022-12-16 16:38:24 +0000

    testprog

--- test/samples/startlights/jacksonproposal.riscv.s
+++ test/samples/startlights/jacksonproposal.riscv.s
@@ -1,23 +1,29 @@
-subroutine_add_light:
-sll a0, a0, 0b1
-addi a0, a0, 0b1
-
 main:
-addi a0, x0, 0b0
-addi x29, x0, 0b111111
+    addi a0, x0, 0b0
+    addi a3, x0, 0b11111111
+    addi a4, x0, 30 #delay
 
 display_lights:
-jal ra, subroutine_add_light
-bne a0, x29, display_lights
+    jal ra, subroutine_add_light
+    bne a0, a3, display_lights
 
 addi a1, x0, 78
 addi a2, x0, 0b0
 
 count_down:
-addi a2, a2, 0b1
-bne a2, a1, count_down
+    addi a2, a2, 0b1
+    bne a2, a1, count_down
 
 addi a0, x0, 0b0
 
 end:
-jal x0, end
+    jal x0, end
+
+subroutine_add_light:
+    addi a5, x0, 0
+    time_out:
+        addi a5, a5, 1
+        bne a5, a4, time_out
+    sll a0, a0, 0b1
+    addi a0, a0, 0b1
+    ret
